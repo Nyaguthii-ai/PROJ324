51635b238927a81647a24d783f3f1d1dab124e2a
Nyaguthii-ai <71819669+Nyaguthii-ai@users.noreply.github.com>
2023-05-15 16:45:23 +0100

    sampling done

--- PROJECT/main.cpp
+++ PROJECT/main.cpp
@@ -41,12 +41,12 @@ DigitalOut led(LED1);
 
 //structure to hold variables for measurement calculations
 struct Sensor {
-    int averages[10];     // Array to store averages
+    float averages[10];     // Array to store averages
     int index;            // Index of the current reading being taken
-    int total;            // Total of all readings taken so far
-    int average;          // Average of all readings taken so far
-    int last_average;     // Average of previous 10 readings
-    int motor_speed;      // Speed of motor vibrations
+    float total;            // Total of all readings taken so far
+    float average;          // Average of all readings taken so far
+    float last_average;     // Average of previous 10 readings
+    float motor_speed;      // Speed of motor vibrations
 };
 
 // Initialize two instances of the Sensor structure
@@ -59,16 +59,28 @@ int motor_speed;
 
 //============================ MEASURE DISTANCES FROM SENSORS ===============================
 void takereadings() {
-    //measure distance from HCSRO4
-    hc_reading = hcsr04.get_dist_cm(); 
-    //measure distance from VL53L0X in cm
-    vl_reading = (vl53l0x.getRangeMillimeters())/10;
+
+    hc.total = 0;
+    vl.total = 0;
+    hc.index = 0;
+    vl.index = 0;
 
     //take ten readings then average them out 
     for (int i = 0; i < 10; i++)
     {
-        hc.total = hc_reading ++; //sum up the ultrasonic values 
-        vl.total = vl_reading ++; //sum up lidar values
+
+        //initialise ultrasonic
+        hcsr04.start();
+        wait_us(5000);
+    
+        //measure distance from HCSRO4
+        hc_reading = hcsr04.get_dist_cm(); 
+        //measure distance from VL53L0X in cm
+        vl_reading = (vl53l0x.getRangeMillimeters())/10;
+
+        hc.total += hc_reading; //sum up the ultrasonic values 
+        vl.total += vl_reading; //sum up lidar values
+
         //update the indexes after counts
         hc.index ++;
         vl.index ++;
@@ -76,15 +88,15 @@ void takereadings() {
 
     //take the averages
     hc.average = hc.total / hc.index;
-    printf("HCSR04 %ucm \n", hc.average);
+    printf("HCSR04 %.3fcm \n", hc.average); //print ultrasonic readings every 10ms
     vl.average = vl.total / vl.index;
-    printf("VL53L0X %ucm \n", hc.average);
+    printf("VL53L0X %.3fcm \n", vl.average); //print lidar
 
     //store avergaes in the array
-    for (int i = 0; i < 10; i++) {
-       hc.average += hc.averages[i];
-       vl.average += vl.averages[i];
-    }
+   // for (int i = 0; i < 10; i++) {
+    //   hc.average += hc.averages[i];
+    //   vl.average += vl.averages[i];
+   // }
 }
 
 //using a thread and semaphore, call the semaphore in the ISR
@@ -108,12 +120,11 @@ void compare() {
 //Creates a queue with the default size
 EventQueue mainQueue;
 EventQueue workerQueue;
+Thread t1;
+Thread t2;
 
 int main() {
 
-    //initialise ultrasonic
-    hcsr04.start();
-    
     //initialise lidar 
     vl53l0x.init();
     vl53l0x.setModeContinuous();
@@ -122,14 +133,20 @@ int main() {
     // Set the initial motor speed
     float speed = 0;
 
+    // Start the event queue
+    t1.start(callback(&mainQueue, &EventQueue::dispatch_forever));
     // Attach functions to event queue
-    mainQueue.call_in(1ms, takereadings);
+    mainQueue.call_every(100ms, takereadings);
     workerQueue.call_in(10ms, compare);
 
-    while (1) {
     
-
+    while (1) {
+        //hc.index = 0;
+        //takereadings();
+        //printf("working \n"); //print ultrasonic readings every 10ms
+        //printf("VL53L0X %ucm \n", hc.average); //print lidar
     }
+    
 }
 
 //*/
